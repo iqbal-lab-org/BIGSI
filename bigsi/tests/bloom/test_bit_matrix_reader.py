ddf6e831df8939979089476be2e8e20367cf0983
Zhicheng-Liu <zl@ebi.ac.uk>
2020-03-29 17:16:11 +0100

    Use pytest.raises

--- bigsi/tests/bloom/test_bit_matrix_reader.py
+++ bigsi/tests/bloom/test_bit_matrix_reader.py
@@ -1,59 +1,57 @@
 import math
+import pytest
 from tempfile import NamedTemporaryFile
 from bitarray import bitarray
 
-from unittest import TestCase
 from hypothesis import given, assume, strategies as st
 
 from bigsi.bloom import BitMatrixReader
 
 
-class BitMatrixReaderTest(TestCase):
+@given(rows=st.integers(min_value=1, max_value=8), cols=st.integers(min_value=1, max_value=8))
+def test_bit_matrix_reader_creation_success(rows, cols):
+    assume(rows * cols <= 8)
 
-    @staticmethod
-    @given(rows=st.integers(min_value=1, max_value=8), cols=st.integers(min_value=1, max_value=8))
-    def test_bit_matrix_reader_creation_success(rows, cols):
-        assume(rows * cols <= 8)
+    with NamedTemporaryFile() as tmp:
+        tmp.write(bytes(1))
+        tmp.flush()
+        with open(tmp.name, "rb") as infile:
+            BitMatrixReader(infile, rows, cols)
 
-        with NamedTemporaryFile() as tmp:
-            tmp.write(bytes(1))
-            tmp.flush()
-            with open(tmp.name, "rb") as infile:
-                BitMatrixReader(infile, rows, cols)
 
-    @given(rows=st.integers(), cols=st.integers())
-    def test_bit_matrix_reader_creation_failure(self, rows, cols):
-        assume(rows * cols > 8 or rows * cols == 0)
+@given(rows=st.integers(), cols=st.integers())
+def test_bit_matrix_reader_creation_failure(rows, cols):
+    assume(rows * cols > 8 or rows * cols == 0)
 
-        with NamedTemporaryFile() as tmp:
-            tmp.write(bytes(1))
-            tmp.flush()
-            with open(tmp.name, "rb") as infile, self.assertRaises(Exception):
-                BitMatrixReader(infile, rows, cols)
+    with NamedTemporaryFile() as tmp:
+        tmp.write(bytes(1))
+        tmp.flush()
+        with open(tmp.name, "rb") as infile, pytest.raises(Exception):
+            BitMatrixReader(infile, rows, cols)
 
-    @staticmethod
-    @given(cols=st.integers(min_value=1, max_value=8),
-           byte_values=st.lists(min_size=1, max_size=100, elements=st.integers(min_value=0, max_value=255)))
-    def test_bit_matrix_reader_iteration_success(cols, byte_values):
-        rows = math.floor(len(byte_values) * 8 / cols)
 
-        bit_array = bitarray()
-        bit_array.frombytes(bytes(byte_values))
-        bit_array.reverse()
-        expected = []
-        for _ in range(rows):
-            this_bit_array = bitarray()
-            for _ in range(cols):
-                this_bit_array.append(bit_array.pop())
-            expected.append(this_bit_array)
+@given(cols=st.integers(min_value=1, max_value=8),
+       byte_values=st.lists(min_size=1, max_size=100, elements=st.integers(min_value=0, max_value=255)))
+def test_bit_matrix_reader_iteration_success(cols, byte_values):
+    rows = math.floor(len(byte_values) * 8 / cols)
 
-        result = []
-        with NamedTemporaryFile() as tmp:
-            tmp.write(bytes(byte_values))
-            tmp.flush()
-            with open(tmp.name, "rb") as infile:
-                bmr = BitMatrixReader(infile, rows, cols)
-                for _ in range(rows):
-                    result.append(next(bmr))
+    bit_array = bitarray()
+    bit_array.frombytes(bytes(byte_values))
+    bit_array.reverse()
+    expected = []
+    for _ in range(rows):
+        this_bit_array = bitarray()
+        for _ in range(cols):
+            this_bit_array.append(bit_array.pop())
+        expected.append(this_bit_array)
 
-        assert result == expected
+    result = []
+    with NamedTemporaryFile() as tmp:
+        tmp.write(bytes(byte_values))
+        tmp.flush()
+        with open(tmp.name, "rb") as infile:
+            bmr = BitMatrixReader(infile, rows, cols)
+            for _ in range(rows):
+                result.append(next(bmr))
+
+    assert result == expected
