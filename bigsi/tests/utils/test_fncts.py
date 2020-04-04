8fcba441e3f2f9a0a1205531a4a4b31933b9ede3
Zhicheng-Liu <zl@ebi.ac.uk>
2020-04-04 21:21:24 +0100

    Fix a bug where non zero positions are incorrect for bitarray
    Since bitarray 0.9.0, the `numpy.where(bitarray)` call does not return
    the correct non zero positions for bits. Instead, it returns the index
    of non zero bytes. For example, for `bitarray("0000000001000000")`, the
    function call returns non zero positions `[1]`, instead of `[9]`, since
    bitarray release 0.9.0.
    
    This change fixes this issue by iterating over the bitarray and returning
    the list of indices at which the element is `True`.

---
+++ bigsi/tests/utils/test_fncts.py
@@ -0,0 +1,17 @@
+import numpy
+from bitarray import bitarray
+from hypothesis import given, strategies as st
+
+from bigsi.utils.fncts import non_zero_bitarrary_positions
+
+
+@given(byte_values=st.lists(min_size=0, max_size=100, elements=st.integers(min_value=0, max_value=255)))
+def test_non_zero_bitarrary_positions_success(byte_values):
+    bit_array = bitarray()
+    bit_array.frombytes(bytes(byte_values))
+
+    expected = numpy.where(numpy.unpackbits(bit_array))[0].tolist()
+
+    result = non_zero_bitarrary_positions(bit_array)
+
+    assert result == expected
