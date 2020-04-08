ac656d6484337e679c6d10dbc4e0d5b909e671a0
Zhicheng-Liu <zl@ebi.ac.uk>
2020-04-08 12:18:21 +0100

    Fix an issue where iterable is used after zip(*iterables)

--- bigsi/cmds/merge_blooms.py
+++ bigsi/cmds/merge_blooms.py
@@ -3,10 +3,10 @@ from bigsi.bloom import BitMatrixWriter
 
 
 def merge_blooms(input_data, num_rows, bloom_matrix_out):
-    _, num_cols_list = zip(*input_data)
+    input_path_list, num_cols_list = zip(*input_data)
     total_cols = sum(map(int, num_cols_list))
     with open(bloom_matrix_out, "wb") as output:
-        with BitMatrixGroupReader(input_data, num_rows) as bmgr,\
+        with BitMatrixGroupReader(zip(input_path_list, num_cols_list), num_rows) as bmgr,\
                 BitMatrixWriter(output, num_rows, total_cols) as bmw:
             for row in bmgr:
                 bmw.write(row)
